752a8f200c91a92b1b474928af851585483a5fb0
Mustafov1 <150002025+Mustafov1@users.noreply.github.com>
2025-06-15 23:48:48 +0300

    v.1

---
+++ index.js
@@ -0,0 +1,18 @@
+const minusbtn = document.getElementById("minusbtn"); 
+const reset = document.getElementById("reset"); 
+const plus = document.getElementById("plus"); 
+const countlabel = document.getElementById("countlabel");
+let count = 0;
+
+plus.addEventListener("click", function() { 
+    count++;
+    countlabel.textContent = count;
+});
+minusbtn.addEventListener("click", function() { 
+    count--;
+    countlabel.textContent = count;
+});
+reset.addEventListener("click", function() { 
+    count = 0;
+    countlabel.textContent = count;
+});
